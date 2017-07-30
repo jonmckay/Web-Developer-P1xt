033ca21f9179a088a725cc683cc201a037f477c0
Jon McKay <jmckay0411@gmail.com>
2017-07-29 21:09:10 -0600

    Add loop example
    Example comparing do/while and while loops.

---
+++ YDKJS-Up-And-Going/loop-example.js
@@ -0,0 +1,18 @@
+while (numOfCustomers > 0)
+{
+	console.log( "How may I help you?" );
+
+	// help the customer...
+
+	numOfCustomers = numOfCustomers - 1;
+}
+
+// versus:
+
+do {
+	console.log( "How may I help you?" );
+
+	// help the customer...
+
+	numOfCustomers = numOfCustomers - 1;
+} while (numOfCustomers > 0);
