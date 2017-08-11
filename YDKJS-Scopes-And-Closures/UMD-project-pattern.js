589727831d08eba61ae7c1a3efc4e62bdc14c3ce
Jon McKay <jmckay0411@gmail.com>
2017-08-10 19:21:38 -0600

    Show a pattern from Universal Module Definition using IIFE

---
+++ YDKJS-Scopes-And-Closures/UMD-project-pattern.js
@@ -0,0 +1,14 @@
+/* 
+	This pattern is used in the UMD (Universal Module Definition)
+
+*/
+
+var a = 2;
+
+(function IIFE( def ) {
+	def( window );
+})(function def ( global ){
+	var a = 3;
+	console.log( a );	// 3
+	console.log( global.a ); // 2
+});
