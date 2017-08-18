f211dc98378bb9f5bbfb2d89799d6c7eefa75f03
Jon McKay <jmckay0411@gmail.com>
2017-08-18 17:32:22 -0600

    Show a simple example of a modern module wrapper function

---
+++ YDKJS-Scopes-And-Closures/modern-modules.js
@@ -0,0 +1,55 @@
+/* 
+	modules[name] = impl.apply(impl, deps) invokes the definition wrapper function for a module,
+	stores the return value, the module's API, into an internal list of module tracked by name.
+*/
+
+
+var MyModules = (function Manager() {
+	var modules = {};
+
+	function define(name, deps, impl) {
+		for (var i = 0; i < deps.length; i++) {
+			deps[i] = modules[deps[i]];
+		}
+		modules[name] = impl.apply(impl, deps);
+	}
+
+	function get(name) {
+		return modules[name];
+	}
+
+	return {
+		define: define,
+		get: get
+	};
+})();
+
+MyModules.define("bar", [], function() {
+	function hello(who) {
+		return "Let me introduce: " + who;
+	}
+
+	return {
+		hello: hello
+	};
+});
+
+MyModules.define("foo", ["bar"], function(bar) {
+	var hungry = "hippo";
+
+	function awesome() {
+		console.log(bar.hello(hungry).toUpperCase());
+	}
+
+	return {
+		awesome: awesome
+	};
+});
+
+var bar = MyModules.get("bar");
+var foo = MyModules.get("foo");
+
+console.log(bar.hello( "hippo" ));	// Let me introduce: hippo
+
+foo.awesome();	// LET ME INTRODUCE: HIPPO
+
