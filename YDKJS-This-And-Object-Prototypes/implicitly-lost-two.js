1516ada373979b704ad227a1f2c831705356e914
Jon McKay <jmckay0411@gmail.com>
2017-08-27 23:37:27 -0600

    Add another example of an implicitly bound function losing its binding

---
+++ YDKJS-This-And-Object-Prototypes/implicitly-lost-two.js
@@ -0,0 +1,24 @@
+/* 
+	This is a more subtle but common way an implicitly bound function
+	loses its binding
+*/
+
+function foo() {
+	console.log(this.a);
+}
+
+function doFoo(fn) {
+	// 'fn' is just another reference to 'foo'
+
+	fn();	// <-- call-site!
+}
+
+var obj = {
+	a: 2,
+	foo: foo
+};
+
+var a = "oops, global";	// 'a' also property on global object
+
+doFoo(obj.foo);	// "oops, global"
+
