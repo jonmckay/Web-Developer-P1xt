24bc09877fc933d06f6431381d385b31f9190cd6
Jon McKay <jmckay0411@gmail.com>
2017-08-29 17:58:30 -0600

    Use a re-usable helper function to create hard-binding

---
+++ YDKJS-This-And-Object-Prototypes/hard-binding-helper.js
@@ -0,0 +1,23 @@
+/* 
+	Another way of using hard-binding is to create a re-usable helper
+*/
+
+function foo(something) {
+	console.log(this.a, something);
+	return this.a + something;
+}
+
+// simple 'bind' helper
+function bind(fn, obj) {
+	return function() {
+		return fn.apply(obj, arguments);
+	};
+}
+
+var obj = {
+	a: 2
+};
+
+var bar = bind(foo, obj);
+var b = bar(3);
+console.log(b);
