1925899b52eadad20f0dec49e087e85d148e2f2a
Jon McKay <jmckay0411@gmail.com>
2017-10-11 19:12:07 -0600

    Start chapter 3 on objects by showing literal object form

---
+++ YDKJS-This-And-Object-Prototypes/Objects/literal-form.js
@@ -0,0 +1,14 @@
+/*
+*	Objects come in two forms: The first of these forms is the declarative or literal form
+*/
+
+// Literal syntax for an object
+var myObj = {
+	key: "value"
+	//...
+};
+
+console.log( myObj );
+
+
+
