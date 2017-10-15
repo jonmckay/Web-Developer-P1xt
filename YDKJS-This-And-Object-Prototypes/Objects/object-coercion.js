d63afd6975434d09baf82b2ddd2fcae7bfee2268
Jon McKay <jmckay0411@gmail.com>
2017-10-14 18:00:22 -0600

    Show example of a literal string being converted to a string Object

---
+++ YDKJS-This-And-Object-Prototypes/Objects/object-coercion.js
@@ -0,0 +1,10 @@
+/*
+*	Use the literal form for a value. The language automatically coerces a primitive to a object when necessary.
+*/
+
+var strPrimitive = "I am a string";
+
+console.log( strPrimitive.length );	// 13
+
+console.log( strPrimitive.charAt( 3 ) );	// "m"
+
