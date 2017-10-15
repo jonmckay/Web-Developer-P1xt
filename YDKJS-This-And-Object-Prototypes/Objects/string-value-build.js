2e619d36f18e80a205b923a31fab8de7c9fb95f9
Jon McKay <jmckay0411@gmail.com>
2017-10-14 18:14:27 -0600

    Programmatically build up the value of a string

---
+++ YDKJS-This-And-Object-Prototypes/Objects/string-value-build.js
@@ -0,0 +1,19 @@
+/*
+*	[ ] syntax uses a strings value to specify the location. The program can programmatically build
+*	up the value of the string.
+*/
+
+var wantA = true;
+var myObject = {
+	a: 2
+};
+
+var idx;
+
+if (wantA) {
+	idx = "a";
+}
+
+// later
+
+console.log( myObject[idx] );	// 2
