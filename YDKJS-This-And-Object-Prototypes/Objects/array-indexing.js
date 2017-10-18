a7e80ad88a47b2f7334c9f941b7e96a8effa4202
Jon McKay <jmckay0411@gmail.com>
2017-10-17 20:34:38 -0600

    Show a quick example of array indexing

---
+++ YDKJS-This-And-Object-Prototypes/Objects/array-indexing.js
@@ -0,0 +1,12 @@
+/*
+*	Arrays also used [] for indexing 
+*/
+
+var myArray = [ "foo", 42, "bar" ];
+
+console.log(myArray.length);	// 3
+
+console.log(myArray[0]);	// "foo"
+
+console.log(myArray[2]);	// "bar"
+
