5ff0bf691ee1a3f237bc79462c621d4984269225
Jon McKay <jmckay0411@gmail.com>
2017-10-31 00:06:06 -0600

    Show a property setter operation

---
+++ YDKJS-This-And-Object-Prototypes/Objects/property-setter.js
@@ -0,0 +1,20 @@
+/*
+*	Properties should be defined with stterss, which override the [[Put]] operation
+*/
+
+var myObject = {
+	// define a getter for 'a'
+	get a() {
+		return this._a_;
+	},
+
+	// define a setter for 'a'
+	set a(val) {
+		this._a_ = val * 2;
+	}
+};
+
+myObject.a = 2;
+
+console.log(myObject.a);	// 4
+
