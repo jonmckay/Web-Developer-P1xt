283010d0a7ca964d7b141e30b0fac37d0c66a87c
Jon McKay <jmckay0411@gmail.com>
2017-11-05 18:42:38 -0700

    A simple example used to show iteration over array indices

---
+++ YDKJS-This-And-Object-Prototypes/Objects/array-index-iteration.js
@@ -0,0 +1,13 @@
+/*
+*	This example is not iterating over the values rather, iterating over the indices using the index to reference
+*	the value, as myArray[i]
+*/
+
+var myArray = [1, 2, 3];
+
+for (var i = 0; i < myArray.length; i++) {
+	console.log(myArray[i]);
+}
+
+// 1, 2, 3
+
