bf8f3883df9800a733aecb32206a0f108ec9de57
Jon McKay <jmckay0411@gmail.com>
2018-01-03 13:04:31 -0700

    Fix program to print both objects a property to show shadowing

--- YDKJS-This-And-Object-Prototypes/Prototypes/shadowing.js
+++ YDKJS-This-And-Object-Prototypes/Prototypes/shadowing.js
@@ -11,8 +11,8 @@ var myObject = Object.create( anotherObject );
 console.log(anotherObject.a);	// 2
 console.log(myObject.a);	// 2
 
-anotherObject.hasOwnProperty( "a" );	// true
-anotherObject.hasOwnProperty( "a" );	// false
+console.log(anotherObject.hasOwnProperty( "a" ));	// true
+console.log(myObject.hasOwnProperty( "a" ));	// false
 
 myObject.a++;	// implicit shadowing
 
