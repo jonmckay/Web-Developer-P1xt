fd4a99f0ad9c243cc7818350a533e757c35759f8
Jon McKay <jmckay0411@gmail.com>
2018-01-15 20:48:27 -0700

    Example of how browsers display values/structures differently

---
+++ YDKJS-This-And-Object-Prototypes/Behavior-Delegation/browser-debugging.js
@@ -0,0 +1,11 @@
+/*
+*	JS doesn't how specific values/structures are displayed to the user, these are handled by the browser. As such, each browser will handle these a little differently
+*/
+
+function Foo() {
+
+}
+
+var a1 = new Foo();
+
+console.log(a1);	// Foo()
