02d2150c5114d18f1b4a34c79fb5d668555e85d3
Jon McKay <jmckay0411@gmail.com>
2018-01-19 23:47:58 -0700

    Show OLOO style example

---
+++ YDKJS-This-And-Object-Prototypes/Behavior-Delegation/oloo-design-pattern.js
@@ -0,0 +1,26 @@
+/*
+*	OLOO (objects linked to other objects) example
+*/
+
+var Foo = {
+	init: function(who) {
+		this.me = who;
+	},
+	identify: function() {
+		return "I am " + this.me;
+	}
+};
+
+var Bar = Object.create( Foo );
+
+Bar.speak = function() {
+	alert( "Hello, " + this.identify() + "." );
+};
+
+var b1 = Object.create( Bar );
+b1.init( "b1" );
+var b2 = Object.create( Bar );
+b2.init( "b2" );
+
+b1.speak();
+b2.speak();
