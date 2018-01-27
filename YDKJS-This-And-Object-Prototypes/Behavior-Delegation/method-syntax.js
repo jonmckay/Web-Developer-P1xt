c9371cd3841bb31904625a94956072433ce376b8
Jon McKay <jmckay0411@gmail.com>
2018-01-26 22:43:45 -0700

    Declare a method without using the function keyword in ES6

---
+++ YDKJS-This-And-Object-Prototypes/Behavior-Delegation/method-syntax.js
@@ -0,0 +1,14 @@
+/*
+*	With ES6, we can declare merthods like the class pattern without using the function keyword.
+*	This enables us to use the OLOO pattern.
+*/
+
+var LoginController = {
+	errrors: [],
+	getUser() { // Look ma, no 'function'!
+		// ...
+	},
+	getPassword() {
+		// ...
+	}
+};
