fc9b956f03ec54aeeb769108298784a78b58eff5
Jon McKay <jmckay0411@gmail.com>
2018-08-20 14:13:10 -0600

    Update closure-with-gc to show block-scoping example

--- YDKJS-Scopes-And-Closures/closure-with-gc.js
+++ YDKJS-Scopes-And-Closures/closure-with-gc.js
@@ -17,3 +17,23 @@ var baz = foo();
 
 baz();   // 2 -- closure 
 
+
+// Garbage collection is another useful reason for block-scoping. In the example below, the click event does not
+// need 'someReallBigData' so a block scope can be placed around it so the GC will take care of it after execution.
+
+function process(data) {
+	// do something interesting
+}
+
+// anything declared inside this block can go away after!
+//{ <-- add these
+	let someReallyBigData = { .. };
+
+	process( someReallyBigData );
+//} <-- for GC
+
+var btn = document.getElementById( "my_button" );
+
+btn.addEventListener( "click", function click(evt){
+	console.log("button clicked");
+}, /*capturingPhase=*/false );
